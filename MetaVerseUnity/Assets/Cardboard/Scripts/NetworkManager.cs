8de4fdf9ea86147b1ac8d719af9c74b3b5cba5dd
unknown <MTC@PC4183.ad.datcon.co.uk>
2015-08-07 09:32:10 +0100

    Update

---
+++ MetaVerseUnity/Assets/Cardboard/Scripts/NetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : MonoBehaviour
+{
+	NetworkClient myClient;
+
+	void Update() {
+		if (Cardboard.SDK != null) {
+			SendInfo (1, Cardboard.SDK.HeadPose);
+		}
+	}
+	
+	public class MyMsgType {
+		public static short Info = MsgType.Highest + 1;
+	};
+	
+	public class InfoMessage : MessageBase
+	{
+		public int id;
+		public Pose3D pose;
+	}
+	
+	public void SendInfo(int personID, Pose3D headPose)
+	{
+		InfoMessage msg = new InfoMessage();
+		msg.id = personID;
+		msg.pose = headPose;
+		
+		NetworkServer.SendToAll(MyMsgType.Info, msg);
+	}
+	
+	// Create a client and connect to the server port
+	public void SetupClient()
+	{
+		myClient = new NetworkClient();
+		myClient.RegisterHandler(MsgType.Connect, OnConnected);
+		myClient.RegisterHandler(MyMsgType.Info, OnInfo);
+		myClient.Connect("10.250.235.162", 3000);
+	}
+	
+	public void OnInfo(NetworkMessage netMsg)
+	{
+		InfoMessage msg = netMsg.ReadMessage<InfoMessage>();
+		Debug.Log("OnInfoMessage: id=" + msg.id + "pose=" + msg.pose);
+	}
+	
+	public void OnConnected(NetworkMessage netMsg)
+	{
+		Debug.Log("Connected to server");
+	}
+}
--- MetaVerseUnity2/Assets/Cardboard/Scene/Teleport.cs
+++ MetaVerseUnity2/Assets/Cardboard/Scene/Teleport.cs
@@ -24,22 +24,15 @@ public class Teleport : MonoBehaviour {
     SetGazedAt(false);
   }
 
-  public void SetGazedAt(bool gazedAt) {
-    GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
-  }
+//  public void SetGazedAt(bool gazedAt) {
+//    GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+//  }
 
-  public void Reset() {
-    transform.localPosition = startingPosition;
+  public void Exit() {
+    Application.Quit();  
   }
 
   public void ToggleVRMode() {
     Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
-  }
-
-  public void TeleportRandomly() {
-    Vector3 direction = Random.onUnitSphere;
-    direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-    float distance = 2 * Random.value + 1.5f;
-    transform.localPosition = direction * distance;
   }
 }
